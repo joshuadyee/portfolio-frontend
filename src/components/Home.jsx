cf87df630c2c46e64b00a6260928dfb51677ebd2
Joshua Yee <joshyee16@gmail.com>
2024-08-12 15:01:04 -0700

    Add title container and ProfilePic component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,14 @@
+import { ProfilePic } from "./ProfilePic"
+
 export const Home = () => {
   return (
     <section className="home">
-      <div >
-        This is the home page 
+      <div className="title_container">
+        <h1>Hello!</h1>
+        <h1>I'm Josh Yee,</h1>
+        <h1>Full-Stack Developer.</h1>
       </div>
+      <ProfilePic />
     </section>
   )
 }  
---
+++ src/components/ProfilePic.jsx
@@ -0,0 +1,7 @@
+export function ProfilePic() {
+  return (
+    <div className="pic">
+        <img className="profile_picture" src="./src/assets/profpic.jpg"/>
+    </div>
+  )
+}
