c9702a35ce02e5470979922d13d8ecefa64482aa
Joshua Yee <joshyee16@gmail.com>
2024-08-13 22:10:43 -0700

    Add landing_screen div to home plus stylings, create Home Buttons component and import below landing_screen, make home section responsive

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,57 +1,60 @@
 import { Cube } from "./Cube"
+import { HomeButtons } from "./HomeButtons"
 
 export const Home = () => {
   return (
     <section className="home">
-      <div className="title_container">
-        <h1>
-          <span className="letter">H</span>
-          <span className="letter">e</span>
-          <span className="letter">y</span>
-          <span className="letter">!</span>
-        </h1>
-        <h1>
-          <span className="letter">I</span>
-          <span className="letter">'</span>
-          <span className="letter last">m</span>
-          <span className="letter">J</span>
-          <span className="letter">o</span>
-          <span className="letter">s</span>
-          <span className="letter last">h</span>
-          <span className="letter">Y</span>
-          <span className="letter">e</span>
-          <span className="letter">e</span>
-          {/* <span className="letter">,</span> */}
-        </h1>
-        <h1>
-          <span className="letter">S</span>
-          <span className="letter">o</span>
-          <span className="letter">f</span>
-          <span className="letter">t</span>
-          <span className="letter">w</span>
-          <span className="letter">a</span>
-          <span className="letter">r</span>
-          <span className="letter last">e</span>
-          <span className="letter">D</span>
-          <span className="letter">e</span>
-          <span className="letter">v</span>
-          <span className="letter">e</span>
-          <span className="letter">l</span>
-          <span className="letter">o</span>
-          <span className="letter">p</span>
-          <span className="letter">e</span>
-          <span className="letter">r</span>
+      <div className="landing_screen">
+        <div className="title_container">
+          <h1>
+            <span className="letter">H</span>
+            <span className="letter">e</span>
+            <span className="letter">y</span>
+            <span className="letter">!</span>
+          </h1>
+          <h1>
+            <span className="letter">I</span>
+            <span className="letter">'</span>
+            <span className="letter last">m</span>
+            <span className="letter">J</span>
+            <span className="letter">o</span>
+            <span className="letter">s</span>
+            <span className="letter last">h</span>
+            <span className="letter">Y</span>
+            <span className="letter">e</span>
+            <span className="letter">e</span>
+          </h1>
+          <h1>
+            <span className="letter">S</span>
+            <span className="letter">o</span>
+            <span className="letter">f</span>
+            <span className="letter">t</span>
+            <span className="letter">w</span>
+            <span className="letter">a</span>
+            <span className="letter">r</span>
+            <span className="letter last">e</span>
+            <span className="letter">D</span>
+            <span className="letter">e</span>
+            <span className="letter">v</span>
+            <span className="letter">e</span>
+            <span className="letter">l</span>
+            <span className="letter">o</span>
+            <span className="letter">p</span>
+            <span className="letter">e</span>
+            <span className="letter">r</span>
+          </h1>
           <hr className="border"/>
-        </h1>
-        <div className="subheading">
-          <h3>Full-Stack</h3>
-          <h3>•</h3>
-          <h3>React</h3>
-          <h3>•</h3>
-          <h3>Ruby on Rails</h3>
+          <div className="subheading">
+            <h3>Full-Stack</h3>
+            <h3>•</h3>
+            <h3>React</h3>
+            <h3>•</h3>
+            <h3>Ruby on Rails</h3>
+          </div>
         </div>
+        <Cube />
       </div>
-      <Cube />
+      <HomeButtons  />
     </section>
   )
 }  
---
+++ src/components/HomeButtons.jsx
@@ -0,0 +1,8 @@
+export const HomeButtons = () => {
+  return (
+    <div className="home_buttons">
+      <a href="#contact" className="button">Contact Me</a>
+      <a href="#about" className="button">About Me</a>
+    </div>
+  )
+}
