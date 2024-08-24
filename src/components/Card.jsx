f1f763300b4633a349e4ed0a004d4c2447c2b1fc
Joshua Yee <joshyee16@gmail.com>
2024-08-23 17:52:27 -0700

    Replace Carousel with just the cards, basic stylings for Card in Portfolio

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,12 @@
-export const Card = ({ title, description, image }) => {
+export const Card = ({ title, description, image, stack }) => {
   return (
     <div className="card">
       <img src={image} alt={title} className="card-image"/>
-      <h2 className="card-title">{title}</h2>
-      <p className="card-description">{description}</p>
+      <div className="card-content">
+        <h2 className="card-title">{title}</h2>
+        <p className="card-description">{description}</p>
+        <p className="card-stack">{stack.join(" ")}</p>
+      </div>
     </div>
   )
 }
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,21 +1,26 @@
-import { Carousel } from "./Carousel"
+import { Card } from './Card';
 
 const projects = [
   {
+    id: 1,
     title: "Cinesthesia",
     description: "A social cataloging website where users can rate, review, and share their thoughts on their favorites films with likeminded members of the community.",
     image: "https://static1.squarespace.com/static/6058f3b0dbb27b03bbd36be9/6058f3f6dbb27b03bbd37503/6058f3f6dbb27b03bbd37530/1614892162972/interstellar.png?format=1500w",
     stack: ["React", "Tailwind CSS", "Ruby on Rails", "PostgreSQL"]
   },
   {
+    id: 2,
     title: "Covid-19 Tracker",
     description: "A web application that allows users to track Covid-19 cases by country.",
     image: "https://www.un.org/sites/un2.un.org/files/field/image/1583952355.1997.jpg",
+    stack: ["React", "Material UI", "Chart.js"]
   },
   {
+    id: 3,
     title: "Weather App",
     description: "A web application that allows users to search for the weather in any city.",
     image: "https://www.bigcountryhomepage.com/wp-content/uploads/sites/56/2019/06/Weather-v2.jpg?w=640&h=360&crop=1",
+    stack: ["React", "Tailwind CSS", "OpenWeather API"]
   }
 ]
 
@@ -24,35 +29,20 @@ export const Portfolio = () => {
     <section className="porfolio" id="portfolio">
       <div className="portfolio-container">
         <h1 className="projects-title">
-          Projects
+          Portfolio
         </h1>
-        <Carousel projects={projects}/>
+        <div className="card-container">
+          {projects.map((project) => (
+            <Card 
+              key={project.id}
+              title={project.title}
+              description={project.description}
+              image={project.image}
+              stack={project.stack}
+            />
+          ))}
+        </div>
       </div>
     </section>
   )
 }
-
-
-
-// .about {
-//   display: flex;
-//   flex-direction: column;
-//   justify-content: flex-start;
-//   align-items: center;
-//   color: inherit;
-//   height: fit-content;
-//   width: 100%;
-//   padding: 2em;
-//   margin: 0;
-//   scroll-behavior: smooth;
-//   position: relative;
-//   padding-top: calc(7.5vh + 1em)
-// }
-
-// .about-container {
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-//   gap: 2em;
-//   padding: 2rem;
-// }
