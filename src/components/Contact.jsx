17151ea653f5789b9b32b2153ed5b1bbc5f26af2
Joshua Yee <joshyee16@gmail.com>
2024-08-27 12:55:37 -0700

    Install emailjs, style Contact text and form UI

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,17 +1,37 @@
+import { Links } from "./Links"
+
 export const Contact = () => {
   return (
     <section className="contact" id="contact">
       <div className="contact-container">
-        <h1>Contact Me</h1>
-        <form action="" className="contact-form">
-          <label htmlFor="name">Name:</label>
-          <input type="text" id="name" name="name" required/>
-          <label htmlFor="email">Email:</label>
-          <input type="email" id="email" name="email" required/>
-          <label htmlFor="message">Message:</label>
-          <textarea name="message" id="message" required></textarea>
-          <button type="submit">Submit</button>
-        </form>
+        <div className="contact-text">
+          <h1>Contact Me</h1>
+          <p>
+            I am interested in projects of any size, especially ambitious ones, so please feel free to reach out with any questions or requests using the form or connect with me on social media. Don’t hesitate to contact me!
+          </p>
+          <Links/>
+        </div>
+        <div className="contact-form">
+          <form action="">
+            <ul>
+              <li className="form-row">
+                <input type="text" id="name" name="name" placeholder="Name" required/>
+                <input type="email" id="email" name="email" placeholder="Email" required/>
+              </li>
+              <li>
+                <input type="text" id="subject" name="subject" placeholder="Subject" required/>
+              </li>
+              <li>
+                <textarea name="message" id="message" placeholder="Message" required></textarea> 
+              </li>
+              <li className="button-container">
+                <button type="submit">
+                  Send Message
+                </button>
+              </li>
+            </ul>
+          </form>
+        </div>
       </div>
     </section>
   )
